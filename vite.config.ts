4f2f9aa81be8d1068d9f5fcc28a3798d07f91dd3
ameni <ameni.bannour.inf@gmail.com>
2024-11-11 01:55:41 +0100

    env config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), ""); // Load all VITE_ env variables
 
   return {
     plugins: [react()],
     define: {
-      "import.meta.env.VITE_TMDB_API_TOKEN": JSON.stringify(
-        env.VITE_TMDB_API_TOKEN
-      ),
+      "import.meta.env": env, // Expose all VITE_ prefixed env variables
     },
   };
 });
